3b62915141a028011edd0d64969e8b50a1589cfb
Krzysztof Uliasz <krzysztof.uliasz@gmail.com>
2019-05-23 22:10:33 +0200

    Added settings handling.

--- Celluar-automaton/src/main/java/gui/GraphicUserInterface.java
+++ Celluar-automaton/src/main/java/gui/GraphicUserInterface.java
@@ -5,6 +5,7 @@ import cell.states.BinaryState;
 import cell.states.UniformStateFactory;
 import game.GameOfLife;
 import game.GameOfLifeHelper;
+import game.Rule256;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -15,20 +16,25 @@ import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import neighbor.MooreNeighborhood;
+import neighbor.Neighborhood1D;
 
 public class GraphicUserInterface extends Application {
 
-    private static int MAIN_APP_HEIGHT = 700;
+    private static final int MAIN_APP_HEIGHT = 700;
 
-    private static int MAIN_APP_WIDTH = 1350;
+    private static final int MAIN_APP_WIDTH = 1350;
 
-    private static int NEXT_STATE_BUTTON_HEIGHT = 50;
+    private static final int NEXT_STATE_BUTTON_HEIGHT = 50;
 
-    private static int NEXT_STATE_BUTTON_WIDTH = 200;
+    private static final int NEXT_STATE_BUTTON_WIDTH = 200;
 
     private static int xCellCount = 10;
 
     private static int yCellCount = 10;
+
+    private static final String GAME_OF_LIFE = "Gra w życie";
+
+    private static final String AUTOMATON_1D = "Automaty jednowymiarowe";
 
     private GridPane mainWindow = new GridPane();
 
@@ -49,6 +55,14 @@ public class GraphicUserInterface extends Application {
     private CheckBox mapWrappingCheckBox = new CheckBox("Zawijanie sąsiedztwa");
 
     private Spinner<Integer> radiusSpinner = new Spinner<Integer>();
+
+    private TextField aliveRulesTextField = new TextField();
+
+    private TextField deadRulesTextField = new TextField();
+
+    private Spinner<Integer> automaton1DRulesSpinner = new Spinner<Integer>();
+
+    private Button settingAcceptButton = new Button();
 
     public GraphicUserInterface(){
     }
@@ -87,15 +101,19 @@ public class GraphicUserInterface extends Application {
         setUpRadiusSpinner();
         //add to new sections new Separator();
         //set alive rules x1 text
+        setUpAliveRulesTextField();
         //set dead rules x1 text
+        setUpDeadRulesTextField();
         //set rules v3 text -> int
+        setUpAutomaton1DRulesSpinner();
         //accept rules
+        setUpSettingAcceptButton();
         mainWindow.add(menuPane, 0, 0);
     }
 
     private void setUpGameComboBox(){
-        selectGameComboBox.getItems().add("Gra w życie");
-        selectGameComboBox.getItems().add("Automaty jednowymiarowe");
+        selectGameComboBox.getItems().add(GAME_OF_LIFE);
+        selectGameComboBox.getItems().add(AUTOMATON_1D);
         selectGameComboBox.getSelectionModel().selectFirst();
         menuPane.add(selectGameComboBox, 0, 0);
     }
@@ -137,15 +155,71 @@ public class GraphicUserInterface extends Application {
                 new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 10, 1);
         radiusSpinner.setValueFactory(valueFactory);
 
-        menuPane.add(radiusLabel, 0, 6);
-        menuPane.add(radiusSpinner, 0, 7);
+        menuPane.add(radiusLabel, 0, 7);
+        menuPane.add(radiusSpinner, 0, 8);
     }
+
+    private void setUpAliveRulesTextField(){
+        Label aliveRulesLabel = new Label("Zasady przeżywania komórki:");
+        aliveRulesTextField.setText("2,3");
+        menuPane.add(aliveRulesLabel, 0, 9);
+        menuPane.add(aliveRulesTextField, 0, 10);
+    }
+
+    private void setUpDeadRulesTextField(){
+        Label deadRulesLabel = new Label("Zasady umierania komórki:");
+        deadRulesTextField.setText("3");
+        menuPane.add(deadRulesLabel, 0, 11);
+        menuPane.add(deadRulesTextField, 0, 12);
+    }
+
+    private void setUpAutomaton1DRulesSpinner(){
+        Label automaton1DRulesLabel = new Label("Zasady gry jednowymiarowej:");
+        SpinnerValueFactory<Integer> valueFactory =
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 255, 128);
+        automaton1DRulesSpinner.setValueFactory(valueFactory);
+
+        menuPane.add(automaton1DRulesLabel, 0, 13);
+        menuPane.add(automaton1DRulesSpinner, 0, 14);
+    }
+
+    private void setUpSettingAcceptButton() {
+        settingAcceptButton.setText("Akceptuj ustawienia");
+        settingAcceptButton.setPrefSize(NEXT_STATE_BUTTON_WIDTH, NEXT_STATE_BUTTON_HEIGHT);
+        addSettingAcceptButtonListener();
+        menuPane.add(settingAcceptButton, 0, 15);
+    }
+
 
     private void addNextStateButtonListener(){
         nextStateButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 board.update();
+            }
+        });
+    }
+
+    private void addSettingAcceptButtonListener(){
+        settingAcceptButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                switch (selectGameComboBox.getSelectionModel().getSelectedItem().toString()){
+                    case GAME_OF_LIFE:
+                        Automaton gameOfLife = createNewGameOfLife((int)widthSlider.getValue(), (int)heightSlider.getValue(),
+                                mapWrappingCheckBox.isSelected(), radiusSpinner.getValue(),
+                                aliveRulesTextField.getText(), deadRulesTextField.getText());
+                        board = new Board(gameOfLife);
+                        mainWindow.add(board.initBoard(), 1,0);
+                    case AUTOMATON_1D:
+                        Automaton automaton1D = new Rule256(new UniformStateFactory(BinaryState.DEAD),
+                                new Neighborhood1D((int)widthSlider.getValue()), (int)widthSlider.getValue(),
+                                automaton1DRulesSpinner.getValue());
+                        board = new Board(automaton1D);
+                        mainWindow.add(board.initBoard(), 1,0);
+                    default:
+                        break;
+                }
             }
         });
     }
